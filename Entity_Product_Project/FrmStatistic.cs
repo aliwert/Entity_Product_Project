a54251c457bf98a8cb27130ea529d610cab2e689
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-13 17:14:50 +0300

    number of city

--- Entity_Product_Project/FrmStatistic.cs
+++ Entity_Product_Project/FrmStatistic.cs
@@ -27,13 +27,16 @@ namespace Entity_Product_Project
             label5.Text = db.Tbl_Customer.Count(a => a.STATUS == true).ToString();
             label7.Text = db.Tbl_Customer.Count(b => b.STATUS == false).ToString();
 
+
+            
             label11.Text = db.Tbl_Product.Sum(a => a.STOCK).ToString();
             label9.Text = db.Tbl_Product.Count(i => i.CATEGORY == 1).ToString();
             label19.Text = db.Tbl_Sales.Sum(c => c.PRICE).ToString();
             label13.Text = (from d in db.Tbl_Product orderby d.PRICE descending select d.PRODUCTID).FirstOrDefault();
             label15.Text = (from x in db.Tbl_Product orderby x.PRICE ascending select x.PRODUCTID).FirstOrDefault();
-            label17.Text = db.Tbl_Customer.Count().ToString();
+            label17.Text = (from i in db.Tbl_Customer select i.CITY).Distinct().Count().ToString();
             label23.Text = db.Tbl_Product.Count(l => l.CATEGORY == 2).ToString();
+
         }
     }
 }
